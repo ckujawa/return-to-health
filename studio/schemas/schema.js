541509dfce1ac25391c7e768aa4f7845b8dcce41
Chris Kujawa <ckujawa@chriskujawadev.com>
2021-02-01 15:55:15 -0500

    fixing broken tags type

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -7,7 +7,6 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 // We import object and document schemas
 import blockContent from './blockContent'
 import category from './category'
-import tag from './tag'
 import post from './post'
 import author from './author'
 import comment from './comment'
@@ -24,7 +23,6 @@ export default createSchema({
     post,
     author,
     category,
-    tag,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
